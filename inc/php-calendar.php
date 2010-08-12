6da6aa64c359a5e2eb72102cf2e1cfed8932388f
rosshanney <rosshanney@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-12 11:51:27 +0000

    updates to php-calendar.php
    git-svn-id: https://plugins.svn.wordpress.org/google-calendar-events/trunk@274765 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/php-calendar.php
+++ inc/php-calendar.php
@@ -8,13 +8,17 @@
 Changes made to original PHP Calendar script by me (Ross Hanney):
 
 - Renamed CSS classes to fit with my plugin
-- Slight modification of lines 57-65 to use Unix timestamp rather than day number
-- Other small markup changes
+- Slight modification of lines 62-70 to use Unix timestamp rather than day number
 - Renamed function to prevent conflicts
-- Replaced strftime with date_i18n on lines 25 and 28 for localization
+- Replaced strftime with date_i18n
+- Use of $wp_locale to get weekday initials
+- Replaced htmlentities() with esc_attr() and esc_html()
+- Other small markup changes
 */
 
 function gce_generate_calendar($year, $month, $days = array(), $day_name_length = 3, $month_href = NULL, $first_day = 0, $pn = array()){
+	global $wp_locale;
+
 	$first_of_month = gmmktime(0, 0, 0, $month, 1, $year);
 	#remember that mktime will automatically correct if invalid dates are entered
 	# for instance, mktime(0,0,0,12,32,1997) will be the date for Jan 1, 1998
@@ -22,12 +26,13 @@ function gce_generate_calendar($year, $month, $days = array(), $day_name_length 
 
 	$day_names = array(); #generate all the day names according to the current locale
 	for($n = 0, $t = (3 + $first_day) * 86400; $n < 7; $n++, $t += 86400){ #January 4, 1970 was a Sunday
-		$day_names[$n] = ucfirst(date_i18n('l', $t));
+		$day_names[$n]['full'] = date_i18n('l', $t);
+		$day_names[$n]['initial'] = $wp_locale->get_weekday_initial(date_i18n('l', $t));
 	}
 
 	list($month, $year, $month_name, $weekday) = explode(',', date_i18n('m, Y, F, w', $first_of_month));
 	$weekday = ($weekday + 7 - $first_day) % 7; #adjust for $first_day
-	$title = htmlentities(ucfirst($month_name)) . '&nbsp;' . $year;  #note that some locales don't capitalize month and day names
+	$title = esc_html($month_name) . '&nbsp;' . $year;  #note that some locales don't capitalize month and day names
 
 	#Begin calendar. Uses a real <caption>. See http://diveintomark.org/archives/2002/07/03
 	@list($p, $pl) = each($pn);
@@ -36,12 +41,12 @@ function gce_generate_calendar($year, $month, $days = array(), $day_name_length 
 	if($p) $p = '<span class="gce-prev">' . ($pl ? '<a class="gce-change-month" title="Previous month" name="' . $pl . '">' . $p . '</a>' : $p) . '</span>&nbsp;';
 	if($n) $n = '&nbsp;<span class="gce-next">' . ($nl ? '<a class="gce-change-month" title="Next month" name="' . $nl . '">' . $n . '</a>' : $n) . '</span>';
 
-	$calendar = '<table class="gce-calendar">' . "\n" . '<caption class="gce-caption">' . $p . '<span class="gce-month-title">' . ($month_href ? '<a href="' . htmlspecialchars($month_href) . '">' . $title . '</a>' : $title) . '</span>' . $n . "</caption>\n<tr>";
+	$calendar = '<table class="gce-calendar">' . "\n" . '<caption class="gce-caption">' . $p . '<span class="gce-month-title">' . ($month_href ? '<a href="' . esc_attr($month_href) . '">' . $title . '</a>' : $title) . '</span>' . $n . "</caption>\n<tr>";
 
 	if($day_name_length){ #if the day names should be shown ($day_name_length > 0)
 		#if day_name_length is >3, the full name of the day will be printed
 		foreach($day_names as $d){
-			$calendar .= '<th abbr="' . htmlentities($d) . '">' . htmlentities($day_name_length < 4 ? substr($d, 0, $day_name_length) : $d) . '</th>';
+			$calendar .= '<th abbr="' . esc_attr($d['full']) . '">' . esc_html($d['initial']) . '</th>';
 		}
 
 		$calendar .= "</tr>\n<tr>";
@@ -58,7 +63,7 @@ function gce_generate_calendar($year, $month, $days = array(), $day_name_length 
 
 		if(isset($days[$timestamp]) and is_array($days[$timestamp])){
 			@list($link, $classes, $content) = $days[$timestamp];
-			$calendar .= '<td' . ($classes ? ' class="' . htmlspecialchars($classes) . '">' : '>') . ($link ? '<a href="' . htmlspecialchars($link) . '"><span class="gce-day-number">' . $day . '</span></a>' . $content : '<span class="gce-day-number">' . $day . '</span>' . $content) . '</td>';
+			$calendar .= '<td' . ($classes ? ' class="' . esc_attr($classes) . '">' : '>') . ($link ? '<a href="' . esc_attr($link) . '"><span class="gce-day-number">' . $day . '</span></a>' . $content : '<span class="gce-day-number">' . $day . '</span>' . $content) . '</td>';
 		}else{
 			$calendar .= '<td><span class="gce-day-number">' . $day . '</span></td>';
 		}
