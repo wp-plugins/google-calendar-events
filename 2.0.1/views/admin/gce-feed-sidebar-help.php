2a1d84e71f238543318ab4fa01a154e94709a38b
pderksen <pderksen@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-11 05:25:30 +0000

    Created tag 2.0.1.
    git-svn-id: https://plugins.svn.wordpress.org/google-calendar-events/tags@986952 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 2.0.1/views/admin/gce-feed-sidebar-help.php
@@ -0,0 +1,27 @@
+<div>
+	<ul>
+		<li>
+			<div class="dashicons dashicons-arrow-right-alt2"></div>
+			<a href="<?php echo gce_ga_campaign_url( 'http://wpdocs.philderksen.com/google-calendar-events/', 'gce_lite', 'sidebar_link', 'docs' ); ?>" target="_blank">
+				<?php _e( 'Documentation & Getting Started', 'gce' ); ?></a>
+		</li>
+
+		<li>
+			<div class="dashicons dashicons-arrow-right-alt2"></div>
+			<a href="http://wordpress.org/support/plugin/google-calendar-events" target="_blank">
+				<?php _e( 'Community Support Forums', 'gce' ); ?></a>
+		</li>
+
+		<li>
+			<div class="dashicons dashicons-arrow-right-alt2"></div>
+			<a href="http://eepurl.com/0_VsT" target="_blank">
+				<?php _e( 'Get Notified of Major Updates', 'gce' ); ?></a>
+		</li>
+
+		<li>
+			<div class="dashicons dashicons-arrow-right-alt2"></div>
+			<a href="http://wordpress.org/support/view/plugin-reviews/google-calendar-events" target="_blank">
+				<?php _e( 'Now Accepting 5-Star Reviews!', 'gce' ); ?></a>
+		</li>
+	</ul>
+</div>
